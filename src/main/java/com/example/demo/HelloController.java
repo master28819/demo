68a58be035c7704403c9e74423006bf9b59cfd88
inj-krish19 <kglivee19@gmail.com>
2024-10-05 21:21:17 +0530

    Testing Thymeleaf Content

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,19 +1,15 @@
 package com.example.demo;
 
+import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
 
     @GetMapping("/hello")
-    public String sayHello() {
-        return "Hello, World!";
+    public String hello() {
+//        model.addAttribute("name", "Spring Boot with Thymeleaf");
+        return "hello"; // refers to hello.html in templates folder
     }
-
-	@GetMapping("/")
-    public String sayRoute() {
-        return "Tera bhai Krish Shah ";
-    }
-
 }
