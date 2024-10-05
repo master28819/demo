79d989f470b353b490dbf33a9a03b1e9adeee8de
inj-krish19 <kglivee19@gmail.com>
2024-10-05 21:41:34 +0530

    Solving Annotation Issue

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,7 +1,7 @@
 package com.example.demo;
 
 // import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.RestController;
+import org.springframework.web.bind.annotation.RestController;
 //import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
